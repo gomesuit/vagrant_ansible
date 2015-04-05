c899520e87fe304aa8199774399ea6a9c6a3e537
gomesuit <gomesuit@gmail.com>
2015-04-05 23:21:22 +0900

    first commit
    first commit

---
+++ provision.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "alias which='alias | /usr/bin/which --tty-only --read-alias --show-dot --show-tilde'" >> /home/vagrant/.bashrc
+
+# init vagrant
+/vagrant/provision/install/init_vagrant.sh
+
+. /etc/global.rc
+
+# yum install
+${SCRIPT_HOME}/install_yum.sh
+
+# install ruby
+#${SCRIPT_HOME}/install_ruby-2.2.0.sh
+
+# service stop
+${SCRIPT_HOME}/stop_service.sh
+
+# set timezone
+${SCRIPT_HOME}/set_timezone.sh

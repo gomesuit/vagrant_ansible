15f0fa2db30c3c697261d36c119ad74acff243bf
gomesuit <gomesuit@gmail.com>
2015-04-06 17:43:39 +0900

    git
    git

--- provision.sh
+++ provision.sh
@@ -27,15 +27,15 @@ yum -y install ansible
 # set ssh-key
 #${SCRIPT_HOME}/set_ssh-key.sh
 
-cd ${USR_HOME}
+cd /home/vagrant
 git clone https://github.com/gomesuit/ansible_Playbook.git
 cd ansible_Playbook
 ansible-playbook -i localhost yum-playbook.yml
 ansible-playbook -i localhost servicestop.yml
 ansible-playbook -i localhost timezone.yml
 ansible-playbook -i localhost serverspec.yml
+#ansible-playbook -i localhost git_serverspec.yml
 
-cd ${USR_HOME}
 git clone https://github.com/gomesuit/serverspecFile.git
 cd serverspecFile
 rake spec

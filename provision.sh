c84aa9abdbbdfa30cb326fdf16b0d385d9fdf936
gomesuit <gomesuit@gmail.com>
2015-04-06 17:19:30 +0900

    リファクタリング
    リファクタリング

--- provision.sh
+++ provision.sh
@@ -2,11 +2,11 @@
 echo "alias which='alias | /usr/bin/which --tty-only --read-alias --show-dot --show-tilde'" >> /home/vagrant/.bashrc
 
 # init vagrant
-cd /vagrant/provision/install/
-sed -i -e 's/\r$//' *
-/vagrant/provision/install/init_vagrant.sh
+#cd /vagrant/provision/install/
+#sed -i -e 's/\r$//' *
+#/vagrant/provision/install/init_vagrant.sh
 
-. /etc/global.rc
+#. /etc/global.rc
 
 # yum install
 #${SCRIPT_HOME}/install_yum.sh
@@ -15,13 +15,14 @@ sed -i -e 's/\r$//' *
 #${SCRIPT_HOME}/install_ruby-2.2.0.sh
 
 # install ansible
-${SCRIPT_HOME}/install_ansible.sh
+#${SCRIPT_HOME}/install_ansible.sh
+yum -y install ansible
 
 # service stop
-${SCRIPT_HOME}/stop_service.sh
+#${SCRIPT_HOME}/stop_service.sh
 
 # set timezone
-${SCRIPT_HOME}/set_timezone.sh
+#${SCRIPT_HOME}/set_timezone.sh
 
 # set ssh-key
 #${SCRIPT_HOME}/set_ssh-key.sh
@@ -29,9 +30,9 @@ ${SCRIPT_HOME}/set_timezone.sh
 cd ${USR_HOME}
 git clone https://github.com/gomesuit/ansible_Playbook.git
 cd ansible_Playbook
-#ansible-playbook -i hosts simple-playbook.yml
-#ansible-playbook -i hosts serverspec.yml
 ansible-playbook -i localhost yum-playbook.yml
+ansible-playbook -i localhost servicestop.yml
+ansible-playbook -i localhost timezone.yml
 ansible-playbook -i localhost serverspec.yml
 
 cd ${USR_HOME}

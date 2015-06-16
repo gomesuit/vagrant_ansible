d9bd7cf855d6371c130a3bc6f4edb88254af86f0
gomesuit <gomesuit@gmail.com>
2015-06-16 17:55:29 +0900

    roles

--- provision.sh
+++ provision.sh
@@ -1,38 +1,20 @@
 #!/bin/sh
 
-# init vagrant
-#cd /vagrant/provision/install/
-#sed -i -e 's/\r$//' *
-#/vagrant/provision/install/init_vagrant.sh
-
-#. /etc/global.rc
-
 # install ansible
 yum -y install ansible
 
-# set ssh-key
-#${SCRIPT_HOME}/set_ssh-key.sh
-
+# git clone playbook
 cd /home/vagrant
 git clone https://github.com/gomesuit/ansible_Playbook.git
 cd ansible_Playbook
 
-# yum install
-ansible-playbook -i localhost yum-playbook.yml
+# run ansbile
+ansible-playbook -i localhost localhost.yml
 
-# service stop
-ansible-playbook -i localhost servicestop.yml
-
-# set timezone
-ansible-playbook -i localhost timezone.yml
-
-# install serverspec
-ansible-playbook -i localhost serverspec.yml
-
-#ansible-playbook -i localhost git_serverspec.yml
-
-# run serverspec
+# git clone serverspec file
 cd /home/vagrant
 git clone https://github.com/gomesuit/serverspecFile.git
+
+# run serverspec
 cd serverspecFile
 rake spec

17e15fb8526fc4836f11f23c2a44e31b7ba9f1d8
gomesuit <gomesuit@gmail.com>
2016-04-30 21:34:56 +0900

    install libffi-devel openssl-devel

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,7 +2,13 @@
 
 # install ansible
 yum -y install wget
-yum -y install python-setuptools gcc python-devel
+yum -y install \
+	python-setuptools \
+	gcc \
+	python-devel \
+	libffi-devel \
+	openssl-devel
+
 cd /tmp
 wget http://releases.ansible.com/ansible/ansible-2.0.2.0.tar.gz
 tar zxf ansible-2.0.2.0.tar.gz
